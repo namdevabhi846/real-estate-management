d4a4df2bb63bca324cda147ecff4b07593d10546
namdevabhi846 <156699740+namdevabhi846@users.noreply.github.com>
2024-06-03 18:26:12 +0530

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   api: {
     server: 'http://localhost:8000/',
-    mapKey: '',
+    mapKey: 'pk.eyJ1IjoiYWJoaXNoZWtuYW1kZXYyMDAzIiwiYSI6ImNsd3l5MGZpZDAxODAybHJhYWx0M2E4MWkifQ.zdcNpq5WYXrLVnhWDRir5g',
     googleAuthClientId: '',
     webSocketUrl: ''
   }
